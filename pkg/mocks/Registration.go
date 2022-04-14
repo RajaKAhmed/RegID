3cd1309d99af99bb4266d884ad4cc6a1905e2276
RajaKAhmed <rajaahmedk@gmail.com>
2022-04-14 10:35:53 -0400

    added Registartion component

--- pkg/mocks/Registration.go
+++ pkg/mocks/Registration.go
@@ -4,7 +4,7 @@ import "github.com/RajaKAhmed/RegID/pkg/models"
 
 var Registration = []models.Registration{
 	{
-		AppID:   "123456",
+		AppID:   123456,
 		AppName: "Telemetry",
 		LOBID:   "DEV",
 		AppEnv:  "POC",
