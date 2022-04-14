5e38517f4c39033c0237681ed27343fa38cf8051
RajaKAhmed <rajaahmedk@gmail.com>
2022-04-14 10:09:16 -0400

    added Registartion component

--- pkg/handlers/GetRegistration.go
+++ pkg/handlers/GetRegistration.go
@@ -3,21 +3,23 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/RajaKAhmed/RegID/pkg/mocks"
+	"github.com/gorilla/mux"
 )
 
 func GetRegistration(w http.ResponseWriter, r *http.Request) {
 	// Read Registration ID
-	// readID := mux.Vars(r)
-	// AppID, _ := strconv.Atoi(readID["AppID"])
+	vars := mux.Vars(r)
+	AppID, _ := strconv.Atoi(vars["AppID"])
 	// Iterate over all Registrations
-	for _, Registration := range mocks.Registration {
-		if Registration.AppID == AppID {
+	for _, register := range mocks.Registration {
+		if register.AppID == AppID {
 			// If registration ID is the same send Info as response
 			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(Registration)
+			json.NewEncoder(w).Encode(register)
 			break
 		}
 	}
